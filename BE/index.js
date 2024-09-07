482369f263b7fcff4d8c24ac009f0a01c3995dc9
Yashi2002 <gyawaliyasmini246@gmail.com>
2024-09-07 23:27:36 +0545

    changes

--- BE/index.js
+++ BE/index.js
@@ -23,7 +23,7 @@ app.post('/verify-code', (req, res) => {
 });
 
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
